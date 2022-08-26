d7ea13533c0791b5c9c2fd6ffe42d76225523ced
Dmitry <arhidiman@mail.ru>
2022-08-26 23:06:54 +0700

    unused vars removed

--- src/components/AppMain/AppMain.tsx
+++ src/components/AppMain/AppMain.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect, useRef, useState, FC, ReactNode } from "react";
+import { FC, ReactNode } from "react";
 import './AppMainStyle.css'
-
-
-
 interface IAppBottom {
     children?:  ReactNode
 }
-
 const  AppBottom: FC<IAppBottom> = ({children})=> {
-
     return(
         <div className="app-main">
             {children}
